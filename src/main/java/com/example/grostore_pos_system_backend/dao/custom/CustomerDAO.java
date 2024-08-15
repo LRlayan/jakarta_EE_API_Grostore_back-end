c73a88eae47ab1df5e57bcc18bbe1affdadb382d
LRLayan <rameshlayan4@gmail.com>
2024-08-15 21:41:40 +0530

    extends CrudDAO

---
+++ src/main/java/com/example/grostore_pos_system_backend/dao/custom/CustomerDAO.java
@@ -0,0 +1,9 @@
+package com.example.grostore_pos_system_backend.dao.custom;
+
+import com.example.grostore_pos_system_backend.dao.CrudDAO;
+import com.example.grostore_pos_system_backend.dao.SuperDAO;
+import com.example.grostore_pos_system_backend.entity.Customer;
+
+public interface CustomerDAO extends CrudDAO<Customer> {
+
+}
