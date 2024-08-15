bee8e962de0557eb988691f26cc905191ce30655
LRLayan <rameshlayan4@gmail.com>
2024-08-15 22:06:36 +0530

    OrderDetailBOImpl

---
+++ src/main/java/com/example/grostore_pos_system_backend/bo/custom/impl/OrderDetailBOImpl.java
@@ -0,0 +1,6 @@
+package com.example.grostore_pos_system_backend.bo.custom.impl;
+
+import com.example.grostore_pos_system_backend.bo.custom.OrderDetailBO;
+
+public class OrderDetailBOImpl implements OrderDetailBO {
+}
