ba40dc2394bc48df9ffdad6b10fef492490097d9
LRLayan <rameshlayan4@gmail.com>
2024-08-15 12:24:50 +0530

    init() and doPost()

--- src/main/java/com/example/grostore_pos_system_backend/controller/CustomerController.java
+++ src/main/java/com/example/grostore_pos_system_backend/controller/CustomerController.java
@@ -1,13 +1,26 @@
 package com.example.grostore_pos_system_backend.controller;
 
+import com.example.grostore_pos_system_backend.dto.CustomerDTO;
+import com.example.grostore_pos_system_backend.persistent.customerImpl.CustomerDataProcess;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+@WebServlet(urlPatterns = "/customer")
 public class CustomerController extends HttpServlet {
+
+    private Connection connection;
+    CustomerDataProcess customerDataProcess = new CustomerDataProcess();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -16,7 +29,24 @@ public class CustomerController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        if (!req.getContentType().toLowerCase().contains("application/json") || req.getContentType() == null){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+        Jsonb jsonb = JsonbBuilder.create();
+        CustomerDTO customerDTO = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+        System.out.println("cusDTO " + customerDTO);
+
+        try(var writer = resp.getWriter()){
+           boolean isSaved = customerDataProcess.saveCustomer(customerDTO , connection);
+           if (isSaved){
+               writer.write("successfully");
+           }else {
+               writer.write("Try Again");
+           }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -31,6 +61,12 @@ public class CustomerController extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        super.init();
+        try{
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/groStorePosSystem");
+            this.connection = pool.getConnection();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
