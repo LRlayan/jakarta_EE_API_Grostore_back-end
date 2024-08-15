e4b06b3a8e1d8877539027f38f8dfd117adcccc5
LRLayan <rameshlayan4@gmail.com>
2024-08-15 22:04:56 +0530

    extends CrudDAO

---
+++ src/main/java/com/example/grostore_pos_system_backend/dao/custom/ItemDAO.java
@@ -0,0 +1,8 @@
+package com.example.grostore_pos_system_backend.dao.custom;
+
+import com.example.grostore_pos_system_backend.dao.CrudDAO;
+import com.example.grostore_pos_system_backend.dao.SuperDAO;
+import com.example.grostore_pos_system_backend.entity.Item;
+
+public interface ItemDAO extends CrudDAO<Item> {
+}
---
+++ src/main/java/com/example/grostore_pos_system_backend/dao/custom/OrderDetailDAO.java
@@ -0,0 +1,8 @@
+package com.example.grostore_pos_system_backend.dao.custom;
+
+import com.example.grostore_pos_system_backend.dao.CrudDAO;
+import com.example.grostore_pos_system_backend.dao.SuperDAO;
+import com.example.grostore_pos_system_backend.entity.OrderDetail;
+
+public interface OrderDetailDAO extends CrudDAO<OrderDetail> {
+}
