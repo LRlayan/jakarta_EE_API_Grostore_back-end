05411d96fadfd560b3f6a24278c6345c66e0738a
LRLayan <rameshlayan4@gmail.com>
2024-08-19 00:46:22 +0530

    crud method

--- src/main/java/com/example/grostore_pos_system_backend/bo/custom/ItemBO.java
+++ src/main/java/com/example/grostore_pos_system_backend/bo/custom/ItemBO.java
@@ -1,6 +1,15 @@
 package com.example.grostore_pos_system_backend.bo.custom;
 
 import com.example.grostore_pos_system_backend.bo.SuperBO;
+import com.example.grostore_pos_system_backend.dto.ItemDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public interface ItemBO extends SuperBO {
+    List<ItemDTO> getAllItem(Connection connection) throws SQLException;
+    boolean updateItem(ItemDTO itemDTO,Connection connection) throws SQLException;
+    boolean saveItem(ItemDTO itemDTO , Connection connection) throws SQLException;
+    boolean deleteItem(String id,Connection connection) throws SQLException;
 }
