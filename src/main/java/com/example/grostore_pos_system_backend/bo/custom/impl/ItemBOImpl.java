326466b7e5ede69381e1821469763857efd059ba
LRLayan <rameshlayan4@gmail.com>
2024-08-19 00:46:59 +0530

    getAllItem() and implement method

--- src/main/java/com/example/grostore_pos_system_backend/bo/custom/impl/ItemBOImpl.java
+++ src/main/java/com/example/grostore_pos_system_backend/bo/custom/impl/ItemBOImpl.java
@@ -1,6 +1,41 @@
 package com.example.grostore_pos_system_backend.bo.custom.impl;
 
 import com.example.grostore_pos_system_backend.bo.custom.ItemBO;
+import com.example.grostore_pos_system_backend.dao.DAOFactory;
+import com.example.grostore_pos_system_backend.dao.custom.ItemDAO;
+import com.example.grostore_pos_system_backend.dto.ItemDTO;
+import com.example.grostore_pos_system_backend.entity.Item;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemBOImpl implements ItemBO {
+    ItemDAO itemDAO = (ItemDAO) DAOFactory.getDAOFactory().DAOTypes(DAOFactory.DAOTypes.iTEM);
+
+    @Override
+    public List<ItemDTO> getAllItem(Connection connection) throws SQLException {
+        List<ItemDTO> itemDTOList = new ArrayList<>();
+        List<Item> itemList = itemDAO.getAll(connection);
+        for (Item item:itemList) {
+            itemDTOList.add(new ItemDTO(item.getItemCode(),item.getItemName(),item.getQTYOnHand(),item.getUnitPrice()));
+        }
+        return itemDTOList;
+    }
+
+    @Override
+    public boolean updateItem(ItemDTO itemDTO, Connection connection) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean saveItem(ItemDTO itemDTO, Connection connection) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean deleteItem(String id, Connection connection) throws SQLException {
+        return false;
+    }
 }
