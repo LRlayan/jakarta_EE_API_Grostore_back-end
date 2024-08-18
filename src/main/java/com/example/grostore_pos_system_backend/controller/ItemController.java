bcc1360f59eef394773d4e52670fc701c0cad061
LRLayan <rameshlayan4@gmail.com>
2024-08-19 00:45:58 +0530

    init() , doGet()

--- src/main/java/com/example/grostore_pos_system_backend/controller/ItemController.java
+++ src/main/java/com/example/grostore_pos_system_backend/controller/ItemController.java
@@ -1,16 +1,41 @@
 package com.example.grostore_pos_system_backend.controller;
 
+import com.example.grostore_pos_system_backend.bo.BOFactory;
+import com.example.grostore_pos_system_backend.bo.custom.ItemBO;
+import com.example.grostore_pos_system_backend.dto.ItemDTO;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
 
+@WebServlet(urlPatterns = "/item")
 public class ItemController extends HttpServlet {
+    private Connection connection;
+    ItemBO itemBO = (ItemBO) BOFactory.getBOFactory().BOTypes(BOFactory.BOTypes.ITEM);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        try(var writer = resp.getWriter()){
+            List<ItemDTO> itemDTOList = itemBO.getAllItem(connection);
+            if (itemDTOList != null){
+                resp.setContentType("application/json");
+                Jsonb jsonb = JsonbBuilder.create();
+                jsonb.toJson(itemDTOList,resp.getWriter());
+            }else {
+                writer.write("Not Available");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -30,6 +55,12 @@ public class ItemController extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        super.init();
+        try{
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/groStorePosSystem");
+            this.connection = pool.getConnection();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
