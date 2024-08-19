03e4d4ff4454a2faef328246127d76ef8cddfc04
LRLayan <rameshlayan4@gmail.com>
2024-08-20 00:00:12 +0530

    doPost()

--- src/main/java/com/example/grostore_pos_system_backend/controller/OrderController.java
+++ src/main/java/com/example/grostore_pos_system_backend/controller/OrderController.java
@@ -1,13 +1,25 @@
 package com.example.grostore_pos_system_backend.controller;
 
+import com.example.grostore_pos_system_backend.bo.BOFactory;
+import com.example.grostore_pos_system_backend.bo.custom.OrderBO;
+import com.example.grostore_pos_system_backend.dto.OrderDTO;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.Connection;
 
+@WebServlet(urlPatterns = "/order")
 public class OrderController extends HttpServlet {
+
+    private Connection connection;
+    OrderBO orderBO = (OrderBO) BOFactory.getBOFactory().BOTypes(BOFactory.BOTypes.ORDER);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
@@ -15,7 +27,22 @@ public class OrderController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        if (req.getContentType().toLowerCase().contains("application/json") || req.getContentType() == null){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+        Jsonb jsonb = JsonbBuilder.create();
+        OrderDTO orderDTO = jsonb.fromJson(req.getReader(), OrderDTO.class);
+        try(var writer = resp.getWriter()){
+            boolean isSaved = orderBO.saveOrder(orderDTO , connection);
+            if (isSaved){
+                writer.write("saved order");
+            }else {
+                writer.write("Please try again");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
