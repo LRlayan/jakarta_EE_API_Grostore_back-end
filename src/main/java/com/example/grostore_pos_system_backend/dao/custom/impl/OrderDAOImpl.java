e7225bf39a5fd4f52b6ce5d2b6cec70c17b421a9
LRLayan <rameshlayan4@gmail.com>
2024-08-20 00:00:20 +0530

    save()

--- src/main/java/com/example/grostore_pos_system_backend/dao/custom/impl/OrderDAOImpl.java
+++ src/main/java/com/example/grostore_pos_system_backend/dao/custom/impl/OrderDAOImpl.java
@@ -1,15 +1,17 @@
 package com.example.grostore_pos_system_backend.dao.custom.impl;
 
+import com.example.grostore_pos_system_backend.dao.SQLUtil;
 import com.example.grostore_pos_system_backend.dao.custom.OrderDAO;
 import com.example.grostore_pos_system_backend.entity.Order;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class OrderDAOImpl implements OrderDAO {
     @Override
-    public boolean save(Order dto , Connection connection) {
-        return false;
+    public boolean save(Order dto , Connection connection) throws SQLException {
+        return SQLUtil.executeQuery("INSERT INTO orders VALUES(?,?,?,?,?,?,?)",connection,dto.getOrderID(),dto.getDate(),dto.getCusId(),dto.getDiscountRate(),dto.getDiscount(),dto.getSubTotal(),dto.getBalance());
     }
 
     @Override
