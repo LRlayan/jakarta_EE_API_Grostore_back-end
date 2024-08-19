6e46331befcf044817937050fa166ef30290b7ef
LRLayan <rameshlayan4@gmail.com>
2024-08-19 23:59:48 +0530

    saveOrder()

--- src/main/java/com/example/grostore_pos_system_backend/bo/custom/OrderBO.java
+++ src/main/java/com/example/grostore_pos_system_backend/bo/custom/OrderBO.java
@@ -1,6 +1,11 @@
 package com.example.grostore_pos_system_backend.bo.custom;
 
 import com.example.grostore_pos_system_backend.bo.SuperBO;
+import com.example.grostore_pos_system_backend.dto.OrderDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public interface OrderBO extends SuperBO {
+    boolean saveOrder(OrderDTO orderDTO , Connection connection) throws SQLException;
 }
