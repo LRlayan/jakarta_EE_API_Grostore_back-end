0466cbe08f1f15ecde41ceb477329a676049749b
LRLayan <rameshlayan4@gmail.com>
2024-08-22 15:28:08 +0530

    getAllOrder()

--- src/main/java/com/example/grostore_pos_system_backend/bo/custom/impl/OrderBOImpl.java
+++ src/main/java/com/example/grostore_pos_system_backend/bo/custom/impl/OrderBOImpl.java
@@ -8,11 +8,23 @@ import com.example.grostore_pos_system_backend.entity.Order;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderBOImpl implements OrderBO {
     OrderDAO orderDAO = (OrderDAO) DAOFactory.getDAOFactory().DAOTypes(DAOFactory.DAOTypes.ORDER);
     @Override
     public boolean saveOrder(OrderDTO orderDTO, Connection connection) throws SQLException {
         return orderDAO.save(new Order(orderDTO.getOrderID(),orderDTO.getDate(),orderDTO.getCusId(),orderDTO.getDiscountRate(), orderDTO.getDiscount(), orderDTO.getSubTotal(), orderDTO.getBalance()) , connection);
+    }
+
+    @Override
+    public List<OrderDTO> getAllOrder(Connection connection) throws SQLException {
+        List<OrderDTO> orderDTOList = new ArrayList<>();
+        List<Order> ordersList = orderDAO.getAll(connection);
+        for (Order orders : ordersList) {
+            orderDTOList.add(new OrderDTO(orders.getOrderID(),orders.getDate(),orders.getCusId(),orders.getDiscountRate(),orders.getDiscount(),orders.getSubTotal(),orders.getBalance()));
+        }
+        return orderDTOList;
     }
 }
