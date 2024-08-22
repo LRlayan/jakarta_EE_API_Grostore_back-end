9d1490132a6ed4a6a4973cebe11623eca4002ceb
LRLayan <rameshlayan4@gmail.com>
2024-08-22 15:27:50 +0530

    getAllOrder()

--- src/main/java/com/example/grostore_pos_system_backend/bo/custom/OrderBO.java
+++ src/main/java/com/example/grostore_pos_system_backend/bo/custom/OrderBO.java
@@ -5,7 +5,10 @@ import com.example.grostore_pos_system_backend.dto.OrderDTO;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 public interface OrderBO extends SuperBO {
     boolean saveOrder(OrderDTO orderDTO , Connection connection) throws SQLException;
+
+    List<OrderDTO> getAllOrder(Connection connection) throws SQLException;
 }
