58d920f072ba7e50335a0240bf9160678bbe77ea
LRLayan <rameshlayan4@gmail.com>
2024-08-25 00:11:12 +0530

    save()

--- src/main/java/com/example/grostore_pos_system_backend/dao/custom/impl/OrderDetailDAOImpl.java
+++ src/main/java/com/example/grostore_pos_system_backend/dao/custom/impl/OrderDetailDAOImpl.java
@@ -1,15 +1,18 @@
 package com.example.grostore_pos_system_backend.dao.custom.impl;
 
+import com.example.grostore_pos_system_backend.dao.SQLUtil;
 import com.example.grostore_pos_system_backend.dao.custom.OrderDetailDAO;
 import com.example.grostore_pos_system_backend.entity.OrderDetail;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class OrderDetailDAOImpl implements OrderDetailDAO {
     @Override
-    public boolean save(OrderDetail dto , Connection connection) {
-        return false;
+    public boolean save(OrderDetail dto , Connection connection) throws SQLException {
+        System.out.println("orderDetailDAO : " + dto);
+        return SQLUtil.executeQuery("INSERT INTO orderDetail (orderId, date, customerId, customerName, customerCity, customerTel, itemCode, itemName, orderQTY, unitPrice) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",connection,dto.getOrderId(),dto.getDate(),dto.getCustomerId(),dto.getCustomerName(),dto.getCustomerCity(),dto.getCustomerTel(),dto.getItemCode(),dto.getItemName(),dto.getOrderQTY(),dto.getUnitPrice());
     }
 
     @Override
